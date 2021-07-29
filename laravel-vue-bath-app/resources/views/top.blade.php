62ac584e95f456453a54a3f54750f6f2c57ec070
baby-0105 <epifa_kikka@icloud.com>
2021-07-29 17:17:43 +0900

    ボタン、tabブロック、レイアウト配置

--- laravel-vue-bath-app/resources/views/top.blade.php
+++ laravel-vue-bath-app/resources/views/top.blade.php
@@ -1,19 +1,35 @@
 @extends('layouts.app')
 @section('content')
-  <div id="toppage" class="top-page">
-    <div class="container">
-      <p class="main-text">さぁ、今日のお風呂を探そう！</p>
-      <h1 class="main-title">OFLog</h1>
-      <form action="" class="search-form">
-        @csrf
-        <select name="prefecture">
-          <option selected>選択してください</option>
-          <option>大阪</option>
-          <option>東京</option>
-        </select>
-        <input type="search" placeholder="キーワードを入力してください">
-        <input type="submit" value="検索する">
-      </form>
+    <div id="toppage" class="top-page">
+        <div class="container">
+            <div class="first-view">
+                <div class="search-btn-area">
+                    <a href="{{ url('post/search') }}">検索する</a>
+                </div>
+                <div class="title-block">
+                    <p class="main-text">さぁ、今日のお風呂を探そう！</p>
+                    <h1 class="main-title">OFLog</h1>
+                </div>
+                <div class="my-post-block">
+                    <a href="{{ url('post/mypost') }}">My投稿</a>
+                </div>
+            </div>
+
+            <div class="recommendation">
+                <div class="tab-area">
+                    @if(Auth::check())
+                        <a href="">お住まいの都道府県で人気のお風呂</a>
+                        <a href="">お住まいの地方で人気のお風呂</a>
+                    @endif
+                    <a href="">人気のお風呂</a>
+                </div>
+                {{-- TODO: コンポーネント作っても良さそう --}}
+                @if(Auth::check())
+                    <div class="my-pref">お住まいの都道府県で人気のお風呂一覧</div>
+                    <div class="my-region">お住まいの地方で人気のお風呂一覧</div>
+                @endif
+                <div class="popular">人気のお風呂一覧</div>
+            </div>
+        </div>
     </div>
-  </div>
 @endsection
