acabe198e2f893b4d2c929585595f57b2a1197de
baby-0105 <epifa_kikka@icloud.com>
2021-08-06 13:44:21 +0900

    【view】投稿 / My投稿一覧ページ実装 #18

--- laravel-vue-bath-app/resources/views/post/index.blade.php
+++ laravel-vue-bath-app/resources/views/post/index.blade.php
@@ -1,3 +1,103 @@
 @extends('app')
 @section('content')
+    <div class="post-index">
+        <div class="title-block">
+            <h2 class="title">投稿一覧</h2>
+            <img class="key-img" src="" alt="鍵マーク">
+        </div>
+        <div class="index">
+            <ul class="bath">
+                <li class="list">
+                    <h4 class="title">- 喜多の湯 -</h4>
+                    <div class="desc">
+                        <div class="info">
+                            <div class="bath-img">
+                                <img class="main-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂の画像">
+                                <div class="sub-img">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                </div>
+                            </div>
+                            <p class="prefecture">大阪府八尾市</p>
+                        </div>
+                        <div class="review">
+                            <p class="thoughts">
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                            </p>
+                            <ul class="review-num">
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="list">
+                    <h4 class="title">- 喜多の湯 -</h4>
+                    <div class="desc">
+                        <div class="info">
+                            <div class="bath-img">
+                                <img class="main-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂の画像">
+                                <div class="sub-img">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                </div>
+                            </div>
+                            <p class="prefecture">大阪府八尾市</p>
+                        </div>
+                        <div class="review">
+                            <p class="thoughts">
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                            </p>
+                            <ul class="review-num">
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="list">
+                    <h4 class="title">- 喜多の湯 -</h4>
+                    <div class="desc">
+                        <div class="info">
+                            <div class="bath-img">
+                                <img class="main-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂の画像">
+                                <div class="sub-img">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                    <img class="bath-img" src="{{ url('svg/bath-mark-light-blue.svg') }}" alt="風呂のサブ画像">
+                                </div>
+                            </div>
+                            <p class="prefecture">大阪府八尾市</p>
+                        </div>
+                        <div class="review">
+                            <p class="thoughts">
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                                よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！よかった！！
+                            </p>
+                            <ul class="review-num">
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                                <li>3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 @endsection
