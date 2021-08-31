4ecaedcd8987416fc3bcb9e3b5cb67467b65666c
baby-0105 <epifa_kikka@icloud.com>
2021-08-31 19:18:17 +0900

    投稿機能テスト #39

---
+++ laravel-vue-bath-app/tests/Unit/Post/MyPostTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Post;
+
+use PHPUnit\Framework\TestCase;
+
+class MyPostTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
