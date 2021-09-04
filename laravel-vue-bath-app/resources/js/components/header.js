a1c36cbf44a67bd4c98b7b3605c5e164ab728381
baby-0105 <epifa_kikka@icloud.com>
2021-09-04 21:54:47 +0900

    【Front】Header 完成 #62

--- laravel-vue-bath-app/resources/js/components/header.js
+++ laravel-vue-bath-app/resources/js/components/header.js
@@ -7,5 +7,13 @@ $(function () {
         $('#nav .link').hover(function() {
             $(this).find('.nav-bg').toggleClass('square-hover');
         });
+
+        // Menuの開閉
+        $('#header #burger').on('click', function() {
+            $('#header #nav').addClass('nav-is-open');
+        });
+        $('#header #closeNav').on('click', function() {
+            $('#header #nav').removeClass('nav-is-open');
+        });
     }
 });
--- laravel-vue-bath-app/resources/views/layouts/components/header.blade.php
+++ laravel-vue-bath-app/resources/views/layouts/components/header.blade.php
@@ -1,43 +1,23 @@
 <header class="header" id="header">
   <div class="container">
     <a href="{{ url('/') }}"><img class="logo" src="{{ asset('img/OFLog-logo.png') }}" alt="ヘッダーのロゴ"></a>
-    <div class="nav" id="nav">
+    <a class="burger" id="burger"><img src="{{ asset('svg/burger-menu.svg') }}" alt="ハンバーガーメニュー"></a>
+  </div>
+  <div class="nav" id="nav">
+    <div class="nav-container">
       @if(Auth::check())
-        <p>{{ Auth::user()->name }}</p>
-        <a class="link" href="{{ route('post.mypost') }}">
-          <span class="text">My投稿</span>
-          <img class="nav-bg" src="{{ asset('svg/square-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link" href="{{ route('post.topost') }}">
-          <span class="text">投稿する</span>
-          <img class="nav-bg" src="{{ asset('svg/square-light-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link" href="{{ route('bath.search') }}">
-          <span class="text">検索する</span>
-          <img class="nav-bg" src="{{ asset('svg/square-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link" href="{{ route('bath.favorite.index') }}">
-          <span class="text">お気に入り</span>
-          <img class="nav-bg" src="{{ asset('svg/square-light-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link" href="{{ route('user.mypage') }}">
-          <span class="text">MyPage</span>
-          <img class="nav-bg" src="{{ asset('svg/square-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link blue-link" href="{{ url('logout') }}">
-          <span class="text">ログアウト</span>
-          <img class="nav-bg" src="{{ asset('svg/square-light-blue.svg') }}" alt="navリンクの背景">
-        </a>
+        {{-- <p>{{ Auth::user()->name }}</p> --}}
+        <a class="link" href="{{ route('post.mypost') }}"> My投稿 </a>
+        <a class="link" href="{{ route('post.topost') }}"> 投稿する </a>
+        <a class="link" href="{{ route('bath.search') }}"> 検索する </a>
+        <a class="link" href="{{ route('bath.favorite.index') }}"> お気に入り </a>
+        <a class="link" href="{{ route('user.mypage') }}"> MyPage </a>
+        <a class="link blue-link" href="{{ url('logout') }}"> ログアウト </a>
       @else
-        <a class="link" href="{{ route('user.register') }}">
-          <span class="text">新規登録</span>
-          <img class="nav-bg" src="{{ asset('svg/square-light-blue.svg') }}" alt="navリンクの背景">
-        </a>
-        <a class="link" href="{{ route('user.login') }}">
-          <span class="text">ログイン</span>
-          <img class="nav-bg" src="{{ asset('svg/square-blue.svg') }}" alt="navリンクの背景">
-        </a>
+        <a class="link" href="{{ route('user.register') }}"> 新規登録 </a>
+        <a class="link" href="{{ route('user.login') }}"> ログイン </a>
       @endif
+      <img class="batsu-icon" id="closeNav" src="{{ asset('svg/batsu.svg') }}" alt="バツ　アイコン">
     </div>
   </div>
 </header>
